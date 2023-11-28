894aa06ae6b76a399631a37d96ccdfc6f4c50bfb
Etozinhachan <116160881+Etozinhachan@users.noreply.github.com>
2023-11-28 11:39:02 +0000

    a

--- ApiTest3/wwwroot/js/script.js
+++ ApiTest3/wwwroot/js/script.js
@@ -2,18 +2,19 @@ const compileButton = document.querySelector('#btn_compile');
 const inputInput = document.querySelector('#input_input');
 const codeInput = document.querySelector('#code_input');
 const codeNotesContainer = document.querySelector('#code__container');
-
-function clearForm(){
+const id = 0
+function clearForm() {
     inputInput.value = '';
     codeInput.value = '';
 }
 
 function displayCodeNoteInForm(codeNote) {
-    inputInput.value = codeNote.value.input;
-    codeInput.value = codeNote.value.code;
+    print(codeNote);
+    //inputInput.value = codeNote.value.input;
+    //codeInput.value = codeNote.value.code;
 }
 
-function getNodeById(id){
+function getNodeById(id) {
     fetch(`api/codeNotes/${id}`)
         .then(data => data.json())
         .then(response => displayCodeNoteInForm(response));
@@ -23,7 +24,7 @@ function populateForm(id) {
     getNodeById(id)
 }
 
-function compile(input, code){
+function compile(input, code) {
 
     const body = {
         input: input,
@@ -37,48 +38,49 @@ function compile(input, code){
             "content-type": "application/json"
         }
     })
-    .then(data => data.json())
-    .then(response => {
-        clearForm();
-        displayCodeNote(response);
-    });
+        .then(data => data.json())
+        .then(response => {
+            clearForm();
+            displayCodeNote(response);
+        });
 }
 
-function displayCodeNote(codeNote){
-    
-    
+function displayCodeNote(codeNote) {
+
+
     let allCodeNotes = codeNotesContainer.innerHTML;
 
-    
+
 
     //depois fazer esse data-id usar o id da codeNote a aver cm aquilo la da database por js
     const codeNoteElement = `
-                                <div class="codeNote" data-id="">
+                                <div class="codeNote" data-id="${id}">
                                     <h3>Input: ${codeNote.value.input}</h3>
                                     <p>Code: ${codeNote.value.code}</p>
                                     <h4>Output: ${codeNote.value.output}</h4>
                                 </div>
                             `;
+    id++;
     if (allCodeNotes.indexOf(codeNoteElement) > -1) {
         alert("That code note already exists, try adding a code note that doesn't exist yet");
         return;
     }
-                            
+
     allCodeNotes += codeNoteElement;
-    
+
     codeNotesContainer.innerHTML = allCodeNotes;
 
     // add event listeners
     document.querySelectorAll('.codeNote').forEach(codeNote => {
         codeNote.addEventListener('click', function () {
             //populateForm(codeNote.dataset.id)
-            // displayCodeNoteInForm(codeNote);
+            displayCodeNoteInForm(codeNote);
             alert('WIP feature.');
         });
     });
 }
 
 
-compileButton.addEventListener('click', function(){
+compileButton.addEventListener('click', function () {
     compile(inputInput.value, codeInput.value);
 });
